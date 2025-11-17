58caffa6afa6db3f976bc99d102e43c5b8a0d2d9
ililio <minecrafterililio@gmail.com>
2025-11-17 17:51:20 +0300

    В общем, поиск по конституции работает

--- сonstitution_search.py
+++ сonstitution_search.py
@@ -8,7 +8,7 @@ import requests
 from bs4 import BeautifulSoup
 import logging
 import re
-from database import constitution_db
+from database_constitution import constitution_db
 from ragsystem import RAGSystem
 
 logging.basicConfig(level=logging.INFO)
